c926f8f9f783950bc0e359afac4f0d6b375badd6
Pallav Jain <pallavjain409@gmail.com>
2019-11-08 08:10:31 +0530

    CC week19 day04

--- part1/src/index.js
+++ part1/src/index.js
@@ -11,7 +11,7 @@ const Header = (props) =>{
 const Content = (props) =>{
     return (
         <div>
-        <p>{props.parts} {props.exercises}</p>
+        <p>{props.part} {props.exercises}</p>
         </div>
     )
 }
@@ -29,20 +29,31 @@ const Total = (props) =>{
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const parts = [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
   
     return (
       <div>
         <Header course = {course} />
-        <Content parts = {part1} exercises = {exercises1} />
-        <Content parts = {part2} exercises = {exercises2} />
-        <Content parts = {part3} exercises = {exercises3} />
-       <Total total = {exercises1 + exercises2 + exercises3} />
+
+         <Content part = {parts[0].name} exercises = {parts[0].exercises} />
+         <Content part = {parts[1].name} exercises = {parts[1].exercises} />
+         <Content part = {parts[2].name} exercises = {parts[2].exercises} />
+        <Total total = {parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+        
+        
       </div>
     )
   }
